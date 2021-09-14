ec98494930e80460c76cb9a883658fec544f9682
tomi <dev.dreissiger@gmail.com>
2021-09-14 19:13:44 +0200

    pwa work 2

---
+++ public/sw-mokus.js
@@ -0,0 +1,66 @@
+
+const ServiceWorkerGlobalScope = this;
+const cacheNameBasis = 'mokus';
+const version = '21.09.14.1';
+const cacheName = `${cacheNameBasis}-v${version}`;
+const urlsToCache = [
+    './images/mokusbufe_bw_64_64.png',
+    './images/mokusbufe_bw_128_128.png',
+    './images/mokusbufe_bw_256_256.png',
+    // './images/mokusbufe_bw_320.jpg',
+    './images/mokusbufe_bw_512_512.png',
+    // './images/mokusbufe_bw_640.jpg',
+    // './images/mokusbufe_bw_1280.jpg',
+    // './images/mokusbufe_bw_1920.jpg',
+    // './images/mokusbufe_bw_2560.jpg',
+
+    './',
+    './index.html',
+
+    './manifest.json',
+];
+
+ServiceWorkerGlobalScope.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches
+            .open(cacheName)
+            .then((cache) => cache.addAll(urlsToCache))
+            .then(() => ServiceWorkerGlobalScope.skipWaiting())
+            .catch((err) => console.log(err))
+    );
+});
+
+ServiceWorkerGlobalScope.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys()
+            .then((keyList) => {
+                return Promise.all(keyList.map((key) => {
+                    if (key.startsWith(cacheNameBasis) && key !== cacheName) {
+                        return caches.delete(key);
+                    }
+                }));
+            })
+            .then(() => ServiceWorkerGlobalScope.clients.claim())
+            .catch((err) => console.log(err))
+    );
+});
+
+ServiceWorkerGlobalScope.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches
+            .open(cacheName)
+            .then((cache) => cache.match(event.request, { ignoreSearch: true })
+                .then((response) => response || fetch(event.request.clone())
+                    .then((response) => {
+                        if (response.ok) {
+                            if (new URL(response.url).origin === location.origin) {
+                                cache.put(event.request, response.clone()).then(() => { });
+                            }
+                        }
+                        return response;
+                    })
+                )
+            )
+            .catch((err) => console.log(err))
+    );
+});
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
-import React, { useState } from 'react'
+import React, { useEffect } from 'react'
 
 export default function App() {
+
+  useEffect(() => {
+    const serviceWorkerContainer = navigator.serviceWorker;
+
+    serviceWorkerContainer.register('sw-mokus.js')
+      .then((serviceWorkerRegistration) => {
+        console.log('Service worker registered.', serviceWorkerRegistration);
+
+        serviceWorkerRegistration.addEventListener('updatefound', (event) => {
+          if (serviceWorkerRegistration.active) {
+            console.log('Service worker update found');
+          }
+        });
+      });
+
+    return () => { };
+  }, []);
+
   return (
-    <div className="bg-red-200">sanyi</div>
+    <div className="bg-red-200">mokus</div>
   )
 }
 
