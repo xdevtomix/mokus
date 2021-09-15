08177e96e91677b54bc196a20f09e8c50935be6c
tomi2 <dev.dreissiger@gmail.com>
2021-09-15 19:04:05 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="w-full h-full m-0 p-0">
       <ServiceWorkerInstallerUpdater />
-      <h1>mókusbüfé</h1>
+      <h1>mókusbüfé2</h1>
     </div>
   )
 }
