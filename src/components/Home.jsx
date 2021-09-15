9c1583611634d85afced766ea3bcc23592de5a92
tomi3 <dev.dreissiger@gmail.com>
2021-09-15 21:38:35 +0200

    background image animated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 
 export default function Home() {
+    const imgRef = useRef(null);
+
+    useEffect(() => {
+        imgRef.current.animate(
+            [
+                { offset: 0.0, transform: 'scale(1)' },
+                { offset: 0.5, transform: 'scale(2)' },
+                { offset: 1.0, transform: 'scale(1)' },
+            ],
+            {
+                delay: 500,
+                duration: 2000,
+            }
+        );
+        return () => { };
+    }, []);
+
     return (
         <div className="w-full h-full">
             <img
+                ref={imgRef}
                 srcSet="
                     ./images/mokusbufe_bw_320.jpg 320w,
                     ./images/mokusbufe_bw_640.jpg 640w,
@@ -12,7 +30,7 @@ export default function Home() {
                     ./images/mokusbufe_bw_2560.jpg 2560w
                 "
                 className="w-full h-full object-cover"
-                alt="Mókusbüfé"/>
+                alt="Mókusbüfé" />
         </div>
     );
 }
