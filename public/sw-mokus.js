0efa88298a6d4f3d3d837c74a1f1f2716f0e4302
tomi3 <dev.dreissiger@gmail.com>
2021-09-16 22:34:37 +0200

    menu work 4

--- public/sw-mokus.js
+++ public/sw-mokus.js
@@ -1,7 +1,7 @@
 
 const ServiceWorkerGlobalScope = this;
 const cacheNameBasis = 'mokus';
-const version = '21.09.16.4';
+const version = '21.09.16.5';
 const cacheName = `${cacheNameBasis}-v${version}`;
 const urlsToCache = [
     './images/mokusbufe_bw_64_64.png',
