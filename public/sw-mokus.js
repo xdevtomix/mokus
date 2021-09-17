95cba35b5a88d7ce5a66b9e6b3998f411f3e3c0d
tomi3 <dev.dreissiger@gmail.com>
2021-09-17 12:05:37 +0200

    menu work 8

--- public/sw-mokus.js
+++ public/sw-mokus.js
@@ -1,7 +1,7 @@
 
 const ServiceWorkerGlobalScope = this;
 const cacheNameBasis = 'mokus';
-const version = '21.09.17.4';
+const version = '21.09.17.5';
 const cacheName = `${cacheNameBasis}-v${version}`;
 const urlsToCache = [
     './images/mokusbufe_bw_64_64.png',
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -49,9 +49,8 @@ export default function Menu({ translations, language, isMenuOpen, setIsMenuOpen
             transition duration-300
             ${isMenuOpen ? 'opacity-100' : 'opacity-0'}
             transform ${isMenuOpen ? 'translate-y-0' : 'translate-y-full'}
-            overscroll-y-contain
         `}>
-            <div>
+            <div className="overscroll-y-contain">
                 {translations.dishes.map((item) => {
                     return (
                         <MenuItem key={item.id} role={item.role}>
