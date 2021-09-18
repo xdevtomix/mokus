e4d855adacc8215f32fc9faa7384a93ebbdc7f5b
tomi3 <dev.dreissiger@gmail.com>
2021-09-18 12:28:46 +0200

    responsive work 2

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -4,7 +4,9 @@ export default function MenuItem({role, children}) {
     return (
         <div className={`
             flex justify-between items-center
-            p-2 text-[13px] leading-[13px] text-black
+            p-2 text-black
+            text-[13px] leading-[13px]
+            alcatel-v:text-lg
             ${role === 'header' ? 'bg-black' : 'bg-white'}
         `}>
             {children}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 module.exports = {
-  mode : 'jit',
+  mode: 'jit',
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      'alcatel-v': '600px',
+      'ipad-v': '768px',
+      'ipad-h': '1024px',
+      'win': '1536px',
+      'laptop': '1920px'
+    },
     extend: {},
   },
   variants: {
