48e7e4a6644ad6dcc644b4bb3b59ce81337ef596
tomi3 <dev.dreissiger@gmail.com>
2021-10-08 17:33:10 +0200

    update test

--- public/sw-mokus.js
+++ public/sw-mokus.js
@@ -1,7 +1,7 @@
 
 const ServiceWorkerGlobalScope = this;
 const cacheNameBasis = 'mokus';
-const version = '21.10.08.2';
+const version = '21.10.08.3';
 const cacheName = `${cacheNameBasis}-v${version}`;
 const urlsToCache = [
     './images/mokusbufe_bw_64_64.png',
