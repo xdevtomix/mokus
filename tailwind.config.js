ac4f2b014381a1f6bee974480b5987a183460d1a
tomi <dev.dreissiger@gmail.com>
2021-12-23 16:24:57 +0100

    tailwind 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     screens: {
