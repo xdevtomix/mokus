3ab7fc006699da1a674640a2dd3736e4edfb85f9
tomi <dev.dreissiger@gmail.com>
2022-07-07 21:55:05 +0200

    mokus - closed

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import { useState } from 'react';
+/* import { useState } from 'react'; */
 
 import ServiceWorkerInstallerUpdater from './components/ServiceWorkerInstallerUpdater';
-import Home from './components/Home';
+/* import Home from './components/Home';
 import Menu from './components/Menu';
 import ImageViewer from './components/ImageViewer';
 
-import translations from './assets/translations.json';
+import translations from './assets/translations.json'; */
 
 export default function App() {
-  const [language, setLanguage] = useState('hu');
+  /* const [language, setLanguage] = useState('hu');
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [selectedDish, setSelectedDish] = useState(null);
+  const [selectedDish, setSelectedDish] = useState(null); */
 
   return (
     <div data-component="app" className="w-full h-full">
       <ServiceWorkerInstallerUpdater />
-      <Home translations={translations} language={language} setLanguage={setLanguage} isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+      {/* <Home translations={translations} language={language} setLanguage={setLanguage} isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
       <Menu translations={translations} language={language} setLanguage={setLanguage} isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} setSelectedDish={setSelectedDish} />
-      <ImageViewer language={language} selectedDish={selectedDish} setSelectedDish={setSelectedDish} />
+      <ImageViewer language={language} selectedDish={selectedDish} setSelectedDish={setSelectedDish} /> */}
     </div>
   )
 }
